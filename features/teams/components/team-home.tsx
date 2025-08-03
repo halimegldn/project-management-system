96bd1c7b7ef3c3ebdc896320beee3cb924c931ed
halimegldn <gildanhalime60@gmail.com>
2025-08-03 16:13:09 +0300

    Takımların üyelerini gösterme.

--- features/teams/components/team-home.tsx
+++ features/teams/components/team-home.tsx
@@ -3,8 +3,9 @@
 import { Teams, User } from "@/lib/generated/prisma";
 import { CreateTeam } from "./create-team";
 import { UserIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
-export function TeamsHome({ teams, users, userRole, userId }: { teams: Teams[], users: User[]; userRole: string; userId: string; }) {
+export function TeamsHome({ teams, users, userRole, userId }: { teams: (Teams & { users: User[] })[]; users: User[]; userRole: string; userId: string; }) {
     return (
         <div className="p-6 flex flex-col gap-6">
             <div className="flex justify-between items-center">
@@ -15,17 +16,40 @@ export function TeamsHome({ teams, users, userRole, userId }: { teams: Teams[], 
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {teams.map((team) => (
-                    <div
-                        key={team.id}
-                        className="border rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white"
-                    >
-                        <div className="flex items-center gap-2 text-blue-600">
-                            <UserIcon className="w-5 h-5" />
-                            <p className="font-medium text-lg">
-                                {team.teamName}
-                            </p>
-                        </div>
-                    </div>
+
+                    <Dialog>
+                        <DialogTrigger className="text-sm text-blue-500 underline">
+                            <div
+                                key={team.id}
+                                className="border rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white"
+                            >
+
+                                <div className="flex items-center justify-start gap-10 text-blue-600">
+                                    <UserIcon className="w-5 h-5" />
+                                    <p className="font-medium text-lg">
+                                        {team.teamName}
+                                    </p>
+                                </div>
+                            </div>
+                        </DialogTrigger>
+
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>{team.teamName} Üyeleri</DialogTitle>
+                                <div>
+                                    {
+                                        team.users && team.users.length > 0 ? (
+                                            team.users.map((user) => (
+                                                <DialogDescription key={user.id}>{user.name}</DialogDescription>
+                                            ))
+                                        ) : (
+                                            <DialogDescription>Bu takımda üye yok</DialogDescription>
+                                        )
+                                    }
+                                </div>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
                 ))}
             </div>
         </div>
